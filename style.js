cc0b02aba46112a3a87be26ccdfcebd4d2a98fad
gokulsundarr <133556658+gokulsundarr@users.noreply.github.com>
2023-06-16 21:26:45 +0530

    Json  resume

---
+++ style.js
@@ -0,0 +1,59 @@
+let myResume={
+    "basics": {
+      "name": "R.GOKUL SUNDAR",
+      "email": "gokulsundarr@gmail.com",
+      "phone": 9629642494,
+      "degree": "B.E","M.E",
+      "location": {
+        "address": "3/66 A.N.Mangalam karripatti",
+        "postalCode": 636106,
+        "city": "Salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          
+          "github":"https://github.com/gokulsundarr"
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "Muthayammal Enginerring College",
+        "department": "Mechanical",
+        "studyType": "fulltime",
+        "batch start year": 2012,
+        "batch end year": 2016,
+        "gpa": 6.83,
+      }
+      {
+        "institution": "Vinayaka Mission's kirupananda enginerring college",
+        "department": "Manufacturing engineering",
+        "studyType": "fulltime",
+        "batch start year": 2021,
+        "batch end year": 2023,
+        "course":completed,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "reading books of various journal,",
+      }
+    ]
+  }
+  console.log(myResume);
